f2668e798af1f29a843c6947bbf133f809418177
Awesomered989 <brandondebeer.bdb@gmail.com>
2021-03-07 20:05:47 +0000

    init

---
+++ chartmaker.py
@@ -0,0 +1,45 @@
+from PIL import Image, ImageDraw
+from PIL import ImageFont
+import numpy as np
+import math
+
+def radchar(data:dict,size:tuple,textcolour):
+    radarchart=Image.new("RGBA",size)
+    rw,rh=radarchart.size
+    chart=Image.new("RGBA",(rw,rh))
+    fnt = ImageFont.truetype("arial", rw//20)
+
+    #data
+    #data={"boobs":5,"face":4,"sexyness":5,
+    #"butt":2,"cutness":1,"fanart":4}
+
+    #math
+    ox,oy=rw//2,rh//2
+    coords=[(0,-1),(0.86, -0.5),(0.86,0.5),(0,1),(-0.86, 0.5),(-0.86,-0.5)]
+    text_coords=[(fnt.size*-1.5,-fnt.size),(0,-fnt.size),(0,0),(fnt.size*-1.25,0),(fnt.size*-3,0),(fnt.size*-3,-fnt.size)]
+    max_radius=rw//3
+    level=max_radius//5
+
+    draw = ImageDraw.Draw(radarchart)
+    draw2 = ImageDraw.Draw(chart)
+    for radius in range(max_radius,0,-level+1):
+        coords2=[(ox+(i[0]*radius),oy+(i[1]*radius)) for i in coords]
+        draw.polygon(coords2, fill=(0,0,0,127), outline="white")
+
+    #draw.text((ox-(fnt.size*2),oy-fnt.size-max_radius), "Hello World", font=fnt, fill=(255,255,255,255))
+
+    display_coords=[]
+    data={key:int(value) for key,value in data.items()}
+    max_value=5#max(list(data.values()))
+    for key,value in data.items():
+        index=list(data).index(key)
+        v=int(max_radius*(value/max_value))
+        display_coords.append((ox+(coords[index][0]*v),oy+(coords[index][1]*v)))
+        pointx,pointy=(coords[index][0]*max_radius)+text_coords[index][0], (coords[index][1]*max_radius)+text_coords[index][1]
+        draw.text((ox+pointx,oy+pointy), key.title(), font=fnt, fill=textcolour)
+
+    draw2.polygon(display_coords, fill=(255,0,255,127), outline="red")
+    radarchart.paste(chart, mask=chart)
+    return radarchart
+
+#radarchart.save("radarchart.png", "PNG", quality=70)
---
+++ main.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw
+from PIL import ImageFont
+import requests
+from chartmaker import radchar
+import csv
+import os
+
+def make_chart(name:str,image_url:str,data:dict):
+    main_img=Image.new("RGBA",(1080,1080),"lightgrey")
+    draw = ImageDraw.Draw(main_img)
+
+    draw.rectangle(((30, 30), (1050, 650)), outline="black",width=3, fill="white")
+
+    #girl="https://pbs.twimg.com/media/EJh_9uEXUAAEEih.jpg"
+    dis_image=Image.open(requests.get(image_url, stream=True).raw)
+    dis_image.putalpha(255)
+    dis_image.thumbnail((1014, 614),Image.ANTIALIAS)
+
+    #data={"boobs":5,"face":4,"sexyness":3,"butt":2,"cuteness":5,"thighs":1}
+    radarchart=radchar(data,(1050//2,1080-660),"black")
+
+    dis_coords=((1080//2)-(dis_image.width//2),(683//2)-(dis_image.height//2))
+
+    main_img.paste(dis_image, dis_coords, mask=dis_image)
+    main_img.paste(radarchart, (1050//4,660), mask=radarchart)
+
+    main_img.save(f"charts/{name}.png", "PNG", quality=70)
+
+with open("pokegirls.csv","r") as csvfile:
+    #row_count = csvfile.read()
+    reader = csv.DictReader(csvfile)
+    rl=list(reader)
+    print(rl)
+    for row_num,row in enumerate(rl):
+        table=f"{'╔':═<10}{'╤':═<10}╗\n"
+        table+=f"\n{'╟':─<10}{'┼':─<10}╢\n".join([f"║{key[:9]:<9}│{str(value)[:9]:<9}║" for key,value in row.items()])
+        table+=f"\n{'╚':═<10}{'╧':═<10}╝"
+        table+=f'''
+{f"{int(100*((row_num+1)/len(rl)))}% done":^21}
+{f"{row_num+1}/{len(rl)} lines":^21}
+{"█"*int(21*((row_num+1)/len(rl))):▒<21}'''
+        os.system("clear")
+        print(table)
+        data={key:value for key,value in row.items() if not key in ["name","image"]}
+        make_chart(str(row_num)+"_"+row["name"],row["image"],data)
+    
