187f0ef6534e9e6818d59fb54e9989cfe2f59a37
Beerman595692 <beerman595692@gmail.com>
2021-03-08 11:54:12 +0000

    added comments

--- chartmaker.py
+++ chartmaker.py
@@ -4,14 +4,11 @@ import numpy as np
 import math
 
 def radchar(data:dict,size:tuple,textcolour):
+    #setup
     radarchart=Image.new("RGBA",size)
     rw,rh=radarchart.size
     chart=Image.new("RGBA",(rw,rh))
     fnt = ImageFont.truetype("arial", rw//20)
-
-    #data
-    #data={"boobs":5,"face":4,"sexyness":5,
-    #"butt":2,"cutness":1,"fanart":4}
 
     #math
     ox,oy=rw//2,rh//2
@@ -20,26 +17,30 @@ def radchar(data:dict,size:tuple,textcolour):
     max_radius=rw//3
     level=max_radius//5
 
+    #draws hexagons
     draw = ImageDraw.Draw(radarchart)
     draw2 = ImageDraw.Draw(chart)
     for radius in range(max_radius,0,-level+1):
         coords2=[(ox+(i[0]*radius),oy+(i[1]*radius)) for i in coords]
         draw.polygon(coords2, fill=(0,0,0,127), outline="white")
 
-    #draw.text((ox-(fnt.size*2),oy-fnt.size-max_radius), "Hello World", font=fnt, fill=(255,255,255,255))
-
+    #creates purple hexagon to display data
     display_coords=[]
     data={key:int(value) for key,value in data.items()}
     max_value=5#max(list(data.values()))
+    #maps coordinates
     for key,value in data.items():
         index=list(data).index(key)
         v=int(max_radius*(value/max_value))
         display_coords.append((ox+(coords[index][0]*v),oy+(coords[index][1]*v)))
         pointx,pointy=(coords[index][0]*max_radius)+text_coords[index][0], (coords[index][1]*max_radius)+text_coords[index][1]
+        #adds in labels for data
         draw.text((ox+pointx,oy+pointy), key.title(), font=fnt, fill=textcolour)
 
+    #draws data hexagon
     draw2.polygon(display_coords, fill=(255,0,255,127), outline="red")
     radarchart.paste(chart, mask=chart)
+    
     return radarchart
 
 #radarchart.save("radarchart.png", "PNG", quality=70)
--- main.py
+++ main.py
@@ -6,32 +6,52 @@ import csv
 import os
 
 def make_chart(name:str,image_url:str,data:dict):
+    #creates background image 
     main_img=Image.new("RGBA",(1080,1080),"lightgrey")
     draw = ImageDraw.Draw(main_img)
 
     draw.rectangle(((30, 30), (1050, 650)), outline="black",width=3, fill="white")
 
-    #girl="https://pbs.twimg.com/media/EJh_9uEXUAAEEih.jpg"
+    #gets image of character from url
     dis_image=Image.open(requests.get(image_url, stream=True).raw)
     dis_image.putalpha(255)
     dis_image.thumbnail((1014, 614),Image.ANTIALIAS)
 
-    #data={"boobs":5,"face":4,"sexyness":3,"butt":2,"cuteness":5,"thighs":1}
+    #creates rader chart image from chartmaker.py
     radarchart=radchar(data,(1050//2,1080-660),"black")
 
+    #sets where to place image of character
     dis_coords=((1080//2)-(dis_image.width//2),(683//2)-(dis_image.height//2))
 
+    #puts image together
     main_img.paste(dis_image, dis_coords, mask=dis_image)
     main_img.paste(radarchart, (1050//4,660), mask=radarchart)
 
+    #saves image
     main_img.save(f"charts/{name}.png", "PNG", quality=70)
 
+
+#Emptys Charts Folder
+folder = 'charts/'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+#Runs thourgh CSV file
 with open("pokegirls.csv","r") as csvfile:
     #row_count = csvfile.read()
     reader = csv.DictReader(csvfile)
     rl=list(reader)
     print(rl)
     for row_num,row in enumerate(rl):
+        #crates table to be printed out on the screen
         table=f"{'╔':═<10}{'╤':═<10}╗\n"
         table+=f"\n{'╟':─<10}{'┼':─<10}╢\n".join([f"║{key[:9].title():<9}│{str(value)[:9].title():<9}║" for key,value in row.items()])
         table+=f"\n{'╚':═<10}{'╧':═<10}╝"
@@ -41,6 +61,8 @@ with open("pokegirls.csv","r") as csvfile:
 {"█"*int(21*((row_num+1)/len(rl))):▒<21}'''
         os.system("clear")
         print(table)
+
+        #makes image
         data={key:value for key,value in row.items() if not key in ["name","image"]}
         make_chart(str(row_num)+"_"+row["name"].title(),row["image"],data)
     
